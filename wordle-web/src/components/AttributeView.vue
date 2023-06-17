38861f601c9eb49d9a377030f7a1b81edaa9bc08
Cayden Kappes <caydenkappes@gmail.com>
2023-06-17 13:38:23 -0700

    changed components

--- wordle-web/src/components/AttributeView.vue
+++ wordle-web/src/components/AttributeView.vue
@@ -1,18 +1,19 @@
 <template>
   <v-card :class="[theme.global.name.value === 'dark' ? 'attributeD' : 'attributeL']">
-    <v-card-title> {{ attribute.attributeType }}: {{ attribute.abilityName }} </v-card-title>
+    <v-card-title> {{ attributeType }}: {{ abilityName }} </v-card-title>
     <v-card-text>
-      <span style="white-space: pre-wrap">{{ attribute.description }}</span>
+      <span style="white-space: pre-wrap">{{ description }}</span>
     </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts" setup>
-import type { CivAttribute } from '@/scripts/civAttribute'
 import { useTheme } from 'vuetify/lib/framework.mjs'
 
 const theme = useTheme()
 const props = defineProps<{
-  attribute: CivAttribute
+  attributeType: string
+  abilityName: string
+  description: string
 }>()
 </script>
